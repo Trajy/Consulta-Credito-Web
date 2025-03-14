c291ab999fb2a572f808b629c072ce988704eefe
Henrique Trajano <henrique.trajano.a@gmail.com>
2025-03-13 22:15:24 -0300

    created feature to search credito by numero da nfse

---
+++ src/app/component/consulta-nfse/consulta-nfse.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConsultaNfseComponent } from './consulta-nfse.component';
+
+describe('ConsultaNfseComponent', () => {
+    let component: ConsultaNfseComponent;
+    let fixture: ComponentFixture<ConsultaNfseComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [ConsultaNfseComponent]
+        })
+            .compileComponents();
+
+        fixture = TestBed.createComponent(ConsultaNfseComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
---
+++ src/app/component/consulta-nfse/consulta-nfse.component.ts
@@ -0,0 +1,84 @@
+import { NgIf } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { debounceTime, distinctUntilChanged, tap } from 'rxjs';
+import { Credito } from '../../model/credito.model';
+import { CreditoService } from '../../service/credito-service/credito.service';
+
+@Component({
+    selector: 'app-consulta-nfse',
+    imports: [NgIf, MatTableModule, MatIconModule, MatToolbarModule, MatInputModule, ReactiveFormsModule],
+    templateUrl: './consulta-nfse.component.html',
+    styleUrl: './consulta-nfse.component.scss'
+})
+export class ConsultaNfseComponent implements AfterViewInit {
+
+    public searchForm: FormControl<string> = new FormControl('') as FormControl<string>;
+    public dataFromApi!: Credito[];
+    public trySubmit: boolean = false;
+    @ViewChild('scrollContainer') public scrollContainer!: ElementRef;
+    public displayedColumns = [
+        'numeroCredito',
+	      'numeroNfse',
+	      'dataConstituicao',
+        'valorIssqn',
+        'tipoCredito',
+	      'simplesNacional',
+	      'aliquota',
+	      'valorFaturado',
+	      'valorDeducao',
+	      'baseCalculo'
+    ];
+    private readonly creditoService: CreditoService = inject(CreditoService)
+    private isDown = false;
+    private startX = 0;
+    private startY = 0;
+    private scrollLeft = 0;
+    private scrollTop = 0;
+
+    ngAfterViewInit(): void {
+        this.searchForm.valueChanges
+            .pipe(debounceTime(500), distinctUntilChanged(), tap(value => console.log(value)))
+            .subscribe(this.searchCreditos.bind(this));
+    }
+
+    onMouseDown(event: MouseEvent): void {
+        this.isDown = true;
+        const container = event.currentTarget as HTMLElement;
+        container.classList.add('active');
+        this.startX = event.pageX - container.offsetLeft;
+        this.startY = event.pageY - container.offsetTop;
+        this.scrollLeft = container.scrollLeft;
+        this.scrollTop = container.scrollTop;
+    }
+
+    onMouseUp(): void {
+        this.isDown = false;
+        document.querySelector('.scroll-container')?.classList.remove('active');
+    }
+
+    onMouseMove(event: MouseEvent): void {
+        if (!this.isDown) return;
+        event.preventDefault();
+        const container = event.currentTarget as HTMLElement;
+        const x = event.pageX - container.offsetLeft;
+        const y = event.pageY - container.offsetTop;
+        const walkX = (x - this.startX) * 2; // Ajuste a sensibilidade no eixo X
+        const walkY = (y - this.startY) * 2; // Ajuste a sensibilidade no eixo Y
+        container.scrollLeft = this.scrollLeft - walkX;
+        container.scrollTop = this.scrollTop - walkY;
+    }
+
+    private searchCreditos(numeroNfse: string): void {
+        this.creditoService.getCreditosByNfse(numeroNfse)
+                .subscribe((response) => {
+                    this.dataFromApi = response;
+                    this.trySubmit = true;
+                });
+    }
+
+}
