30e8675ac135eff597fecb3e6adbdcbd713495f4
Henrique Trajano <henrique.trajano.a@gmail.com>
2025-03-13 22:12:32 -0300

    created Credito data model

---
+++ src/app/model/credito.model.ts
@@ -0,0 +1,12 @@
+export interface Credito {
+    numeroCredito: string,
+    numeroNfse: string,
+    dataConstituicao: string,
+    valorIssqn: string,
+    tipoCredito: string,
+    simplesNacional: string,
+    aliquota: number,
+    valorFaturado: number,
+    valorDeducao: number,
+    baseCalculo: number
+}
